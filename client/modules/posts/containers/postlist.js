2a58bf3285b7e2aaf7e99db723a04555085ecb0b
Your Name <you@example.com>
2016-02-12 14:23:24 +0000

    posts module from Mantra sample working with new publication

--- client/modules/posts/containers/postlist.js
+++ client/modules/posts/containers/postlist.js
@@ -1,8 +1,44 @@
-import PostList from '../components/postlist.jsx';
+import PostList from '../components/postlist.jsx'
+//import FeedList from '../../../core/components/FeedList.jsx';
 import {useDeps, composeWithTracker, composeAll} from 'mantra-core';
+import FeedDomain from '../../../core/actions/feed_domain.jsx'
 
 export const composer = ({context}, onData) => {
+  const {Meteor, Collections} = context();
+  sweetAlert("title", "test")
+  const fields = {
+    posts: {
+      _id: true,
+      title: true,
+      likecount: true,
+      commentcount: true,
+      username: true,
+      created: true,
+      owner: true,
+    },
+    postComments: {
+      _id: true,
+      created: true,
+      username: true,
+      description: true,
+      post: true,
+    }
+  }
+  let recordCount = {posts: 5}
+  sweetAlert("fields.posts", Object.keys(fields.posts))
+  sweetAlert("FeedDomain", Object.keys(FeedDomain))
+  const feedposts = FeedDomain.getAllFeedPosts()
+  sweetAlert("feedposts", feedposts)
+  const postIds = FeedDomain.getPostCommentIds()
+  sweetAlert("postIds", postIds)
+  if (Meteor.subscribe("feed", fields, recordCount, postIds).ready()) {
+    const posts = Collections.Posts.find().fetch();
+    sweetAlert("success", Object.keys(posts[1]))
+    onData(null, {posts});
+  }
 
+
+/*
   const fields = {
     posts: {
       _id: true,
@@ -30,6 +66,7 @@ export const composer = ({context}, onData) => {
     const posts = Collections.Posts.find().fetch();
     onData(null, {posts});
   }
+*/
 };
 
 export default composeAll(
