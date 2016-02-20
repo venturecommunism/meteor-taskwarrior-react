e35cba71c6a52aa585bce95a26ad6d1a0866a064
Your Name <you@example.com>
2016-02-20 22:06:17 +0000

    cleanup

--- client/modules/feed/components/_feed.jsx
+++ client/modules/feed/components/_feed.jsx
@@ -1,47 +1,4 @@
-/*
-
 import React from 'react'
-import FeedList from './FeedList.jsx'
-import FeedItemHeader from './FeedItemHeader.jsx'
-import FeedItemFooter from './FeedItemFooter.jsx'
-
-const FeedItem = ({task}) => (
-  <div className='feed-item'>
-    <FeedItemHeader task={task} />
-
-    <div className='feed-item-description'>
-      {task.description}
-    </div>
-
-    <div className='feed-item-entry'>
-      {task.entry}
-    </div>
-
-    <div className='feed-item-status'>
-      {task.status}
-    </div>
-
-    <div className='feed-item-uuid'>
-      {task.uuid}
-    </div>
-
-    <FeedItemFooter task={task} />
-  </div>
-);
-FeedItem.propTypes = {
-  description: React.PropTypes.string.isRequired,
-  taskcomments: React.PropTypes.array.isRequired,
-  uuid: React.PropTypes.string.isRequired,
-  status: React.PropTypes.string.isRequired,
-  entry: React.PropTypes.string.isRequired,
-};
-
-export default FeedItem
-
-*/
-
-import React from 'react';
-// import DataComposer from '../composers/TasksCollection.jsx';
 
 import FeedItemHeader from './FeedItemHeader.jsx'
 import FeedItemFooter from './FeedItemFooter.jsx'
@@ -77,3 +34,16 @@ export default ({collection}) => (
     </button>
   </div>
 );
+
+/*
+FeedItem.propTypes = {
+  description: React.PropTypes.string.isRequired,
+  taskcomments: React.PropTypes.array.isRequired,
+  uuid: React.PropTypes.string.isRequired,
+  status: React.PropTypes.string.isRequired,
+  entry: React.PropTypes.string.isRequired,
+};
+
+export default FeedItem
+*/
+
