8de5bd48b1790091394419bf880f0ad42bb60807
Your Name <you@example.com>
2016-02-20 05:23:00 +0000

    changing server methods to be like _color

--- server/methods/tasks.js
+++ server/methods/tasks.js
@@ -1,69 +1,53 @@
-import {Tasks, TaskComments} from '/lib/feed';
+import {_tasks} from '/lib/collections';
+// import {_task} from '/lib/task.js';
 import {Meteor} from 'meteor/meteor';
 import {check} from 'meteor/check';
-
-// export default function () {
-  Meteor.methods({
-    'tasks.create'(_id, description) {
-      check(_id, String);
-      check(description, String);
-
-      // Show the latency compensations
-      // Meteor._sleepForMs(500);
-
-      // XXX: Do some user authorization
-      const uuid = guid()
-      const createdAt = new Date();
-      const entry = formattedMoment(moment(createdAt))
-      const status = "pending"
-      const task = {_id, description, uuid, createdAt, entry, status};
-      Tasks.insert(task);
-    }
-  });
-
-  Meteor.methods({
-    'tasks.createTaskComment'(_id, taskId, text) {
-      check(_id, String);
-      check(taskId, String);
-      check(text, String);
-
-      // Show the latency compensations
-      // Meteor._sleepForMs(500);
-
-      // XXX: Do some user authorization
-      const createdAt = new Date();
-      const author = 'The User';
-      const taskcomment = {_id, taskId, author, text, createdAt};
-      TaskComments.insert(taskcomment);
-    }
-  });
-//}
+// import _ from 'lodash';
 
 Meteor.methods({
-  'tasks.update'(data, _id) {
+
+  '_tasks.add'(data, _id) {
     check(data, {
-      type: Match.Optional(String),
-      description: Match.Optional(String),
-      uuid: Match.Optional(String)
+      title: String,
+      content: String
     });
     check(_id, String);
 
-    // console.log ('Tasks.update _id', _id);
-    // console.log ('Tasks.update data', data);
+    // console.log('_tasks.add data', data);
 
     // XXX: Do some user authorization
 
-    Tasks.update({_id: _id}, {$set: {type: data.type}})
+    data._id = _id;
+    data.createAt = new Date();
+    // const object = {_id, data.title, data.content, createdAt};
+    _tasks.insert(data);
+  },
 
-/*
-    let record = Tasks.findOne(_id);
-    console.log(record)
-    const allowedFields = [ 'type','description','uuid' ];
+  '_tasks.update'(data, _id) {
+    check(data, {
+      title: String,
+      content: String
+    });
+    check(_id, String);
+
+    // console.log ('_tasks.update _id', _id);
+    // console.log ('_tasks.update data', data);
+
+    // XXX: Do some user authorization
+
+    let record = _tasks.findOne(_id);
+    const allowedFields = [ 'title','content' ];
     allowedFields.forEach(key => record.set(key,data[key]) );
     record.save(allowedFields);
-*/
 
-    // console.log ('Tasks.update record', record);
+    // console.log ('_tasks.update record', record);
 
   },
-})
+
+  '_tasks.delete'(_id) {
+    check(_id, String);
+    //  console.log('_tasks.delete _id', _id);
+    let record = _tasks.findOne(_id);
+    record.remove();
+  }
+});
