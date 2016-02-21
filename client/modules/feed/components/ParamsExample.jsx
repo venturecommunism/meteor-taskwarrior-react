cfbfdcdeb378221faa7cd9af8ed64461c0d2aa41
Your Name <you@example.com>
2016-02-21 22:01:07 +0000

    {projects} concept doesn't exist yet

--- client/modules/feed/components/ParamsExample.jsx
+++ client/modules/feed/components/ParamsExample.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import FeedActions from '../actions/feed_actions'
 import FeedDomain from '../actions/feed_domain.jsx'
 
-export default ({projects}) => (
+export default () => (
   <div className='params-example'>
 
     <button onClick={ FeedActions.filterByProjects }>
