5f29c389818fa0af2c60eb4a20a5aa14f90d36e7
Your Name <you@example.com>
2016-04-16 01:06:40 +0000

    sorting the right way

--- client/modules/feed/state/previouscalendarquery.js
+++ client/modules/feed/state/previouscalendarquery.js
@@ -4,7 +4,7 @@ const feedquery = (context, actions) => ({
   collection: 'tasksbacklog',
   query: {status: "completed", $and: [{tags: {$ne: "inbox"}}, {project: {$exists: false}}, {context: {$exists: false}}]},
   pubsort: {due: -1},
-  subsort: {due: -1},
+  subsort: {due: 1},
   limit: { tasksbacklog: 5 },
   taskids: FeedDomain.getTaskCommentIds(),
   context: () => context,
