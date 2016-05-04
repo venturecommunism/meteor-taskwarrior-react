61f17da9d63f12f7296bbefb8aa82b80aeec530d
Your Name <you@example.com>
2016-05-04 08:08:50 +0000

    cleanup

--- server/main.js
+++
@@ -1,6 +0,0 @@
-import addInitialData from './configs/initial_adds.js';
-import addInitialFeedData from './configs/initial_feed_adds.js'
-import addInitialUsers from './configs/initial_users.js';
-addInitialData();
-addInitialFeedData()
-addInitialUsers();
--- server/methods/index.js
+++ server/methods/index.js
@@ -1,11 +1,9 @@
-import posts from './posts';
 import tasks from './tasks'
 import home from './home'
 import timer from './timer'
 
 export default function () {
   tasks()
-  posts()
   home()
   timer()
 }
