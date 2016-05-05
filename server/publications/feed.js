fb320c8d7deaf3043bb71d5c97ed691a55483fc0
Your Name <you@example.com>
2016-05-05 02:14:19 +0000

    permissioning on publication

--- server/publications/feed.js
+++ server/publications/feed.js
@@ -15,25 +15,18 @@ export default function () {
     console.log('Publishing Tasks', fields)
     console.log('Limit:', limits)
 
-    // SECURITY NOTE
-    // if this was data that could not be shown to a specific set of
-    // users or a logged out user, you would check here to verify they're
-    // allowed to receive the data.
-    // **TRUST NOTHING FROM THE CLIENT** instead ask the server what they're
-    // user ID is and check their permissions to see if the role is met, never
-    // never pass in the user ID from the client as they could guess an admin
-    // ID and gain access.
-    //
-      if (!this.userId)
-           throw new Meteor.Error("401", "Access denied, please login");
-    //        or
-    //  if (Roles.userIsInRole(this.userId, 'admin'))
-    //       throw new Meteor.Error(403, "Not authorized to view this data");
-    // -----------------------------------------------------------------------
+    try {
+      if (!this.userId) {
+        throw 'no userid'
+      } else if (!adminUser(this.userId)) {
+        throw 'not admin user'
+      }
+    } catch (err) {
+      console.log("here", err) // we log the standard JS error
+      err == 'no userid' ? this.error(new Meteor.Error(401, 'Access denied. Please log in')) : ''
+      err == 'not admin user' ? this.error(new Meteor.Error(403, 'Not authorized to view this data')) : ''
+    }
 
-    // ensure *only* the fields we whitelist are passed in. Unless wrapped in
-    // Match.Optional it will be required. If any key does not match the
-    // publication will fail and throw an error
     check(fields, {
       tasks: {
         _id: Boolean,  // id required for security
@@ -57,24 +50,13 @@ export default function () {
 
     var userId = this.userId
     if (adminUser(userId)) {
-      // Mongo Cursors
       const taskscursor = tasks.find(query, {fields: fields.tasks, sort: sort, limit: limits.tasks})
       const taskspendingcursor = taskspending.find(query, {fields: fields.tasks, sort: sort, limit: limits.taskspending})
       const tasksbacklogcursor = tasksbacklog.find(query, {fields: fields.tasks, sort: sort, limit: limits.tasksbacklog})
-
-      // Cursor Array
       let cursors = []
-      if (limits.tasks) {
-        cursors.push(taskscursor)
-      }
-      if (limits.taskspending) {
-        cursors.push(taskspendingcursor)
-      }
-      if (limits.tasksbacklog) {
-        cursors.push(tasksbacklogcursor)
-      }
-
-      // Return Mongo Cursor Array as a Reactive Join
+      limits.tasks ? cursors.push(taskscursor) : ''
+      limits.taskspending ? cursors.push(taskspendingcursor) : ''
+      limits.tasksbacklog ? cursors.push(tasksbacklogcursor) : ''
       return cursors
     }
   })
