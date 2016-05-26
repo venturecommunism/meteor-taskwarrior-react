5ffeb2d56f01ffba2f7456e6e4cb1d3bffdfda59
Your Name <you@example.com>
2016-05-26 11:57:39 +0000

    cleaning up schema and resolvers

--- server/resolvers/feed.js
+++ server/resolvers/feed.js
@@ -4,7 +4,8 @@ import { tasks, taskspending, tasksbacklog, tmpmutation } from '/lib/collections
 export const feed = {
 
   Query: {
-    async metaquery(root, args, context) {
+    async query(root, args, context) {
+      let errors = []
       let limit = args.limit
       delete args.limit
       let skip = args.skip
@@ -15,62 +16,12 @@ export const feed = {
       metaquery.count = cursor.count()
       metaquery.return = cursor.fetch()
       metaquery.subtotal = metaquery.return.length
+      metaquery.errors = errors
       return metaquery
     },
-    async query(root, args, context) {
+    async _query(root, args, context) {
       let selector = JSON.parse(JSONize(args.selector))
-      console.log("selector", selector, typeof selector)
       return Mongo.Collection.get(args.collection).find(selector).fetch()
-    },
-    feed(root, args, context) {
-      var limit = args.limit
-      delete args.limit
-      var skip = args.skip
-      delete args.skip
-      console.log(args)
-      console.log(limit)
-      return tasks.find(args, {limit: limit, skip: skip}).fetch();
-    },
-    oldfeed(root, args, context) {
-      var limit = args.limit
-      delete args.limit
-      var skip = args.skip
-      delete args.skip
-      if (args.dueafter) {
-        var dueafter = args.dueafter
-        delete args.dueafter
-        console.log(args)
-        return taskspending.find({due: {$gte: dueafter}}, {limit: limit, skip: skip, sort: {due: 1}}).fetch()
-      }
-      if (args.duebefore) {
-        var duebefore = args.duebefore
-        delete args.duebefore
-        console.log(args)
-        return taskspending.find({due: {$lte: duebefore}}, {limit: limit, skip: skip, sort: {due: -1}}).fetch()
-      }
-      console.log(args)
-      console.log(limit)
-      return taskspending.find(args, {limit: limit, skip: skip}).fetch()
-    },
-    backlogfeed(root, args, context) {
-      if (args.duebefore) {
-        var limit = args.limit
-        delete args.limit
-        var duebefore = args.duebefore
-        delete args.duebefore
-        console.log(args)
-        return tasksbacklog.find({due: {$lte: duebefore}, status: "completed", $and: [{tags: {$ne: "inbox"}}, {project: {$exists: false}}, {context: {$exists: false}}]}, {limit: limit, sort: {due: -1}}).fetch()
-      }
-      console.log(args)
-      console.log(args.limit)
-      return tasksbacklog.find(args, {limit: limit}).fetch()
-    },
-    count(root, args, context) {
-      console.log("count")
-      return taskspending.find({})
-    },
-    newcount(root, args, context) {
-      return tasks.find({})
     },
     user(root, args, context) {
       // Only return the current user, for security
@@ -86,12 +37,12 @@ export const feed = {
     return: (root) => root.return
   },
   Mutation: {
-    async metamutate(root, args) {
+    async mutate(root, args) {
       console.log(root, args)
       let errors = []
       let selector = JSON.parse(JSONize(args.selector))
       let data = Mongo.Collection.get(args.collection).find(selector).fetch()
-      let out = data.map( function(item) {
+      let inputpipe = data.map( function(item) {
         return item._id
       })
       switch (args.op) {
@@ -103,9 +54,9 @@ export const feed = {
             errors.push(...['mutator', 'No mutator'])
           }
           console.log("data", data)
-          args.count = out.length
+          args.count = inputpipe.length
           args.errors = errors
-          args.out = out
+          args.in = inputpipe
           return args
           break
         case 'update':
@@ -117,9 +68,9 @@ export const feed = {
             errors.push(...['mutator', 'No mutator'])
           }
           console.log("data", data)
-          args.count = out.length
+          args.count = inputpipe.length
           args.errors = errors
-          args.out = out
+          args.in = inputpipe
           return args
           break
         case 'remove':
@@ -131,9 +82,9 @@ export const feed = {
             errors.push(...['mutator', 'No mutator'])
           }
           console.log("data", data)
-          args.count = out.length
+          args.count = inputpipe.length
           args.errors = errors
-          args.out = out
+          args.in = inputpipe
           return args
           break
         default:
@@ -142,61 +93,16 @@ export const feed = {
           return args
       }
     },
-    async mutate(root, args) {
+    async _mutate(root, args) {
       let errors = []
       let selector = JSON.parse(JSONize(args.selector))
       let data = Mongo.Collection.get(args.collection).find(selector).fetch()
-      let out = data.map( function(item) {
+      let inputpipe = data.map( function(item) {
         return item._id
       })
       return Mongo.Collection.get(args.collection).find(selector).fetch()
     },
-    async feedinsert(root, args) {
-      console.log("insert mutation")
-      let id = tmpmutation.insert(args)
-      return tmpmutation.findOne({_id: id})
-    },
-    async feedupdate(root, args) {
-      console.log("update mutation")
-      let returnvalue = tmpmutation.update(args, args)
-      console.log(returnvalue)
-      return returnvalue
-    },
   },
-  Count: {
-    total: () => taskspending.find().count(),
-    projects: () => taskspending.find({tags: "largeroutcome"}).count(),
-    somedaymaybeproj: () => taskspending.find({tags: "somedaymaybeproj"}).count(),
-    somedaymaybecont: () => taskspending.find({tags: "somedaymaybecont"}).count(),
-    somedaymaybeprojproject: () => taskspending.find({tags: { $all: ["largeroutcome", "somedaymaybeproj"]}}).count(),
-    somedaymaybecontcontext: () => taskspending.find({tags: { $all: ["largercontext", "somedaymaybecont"]}}).count(),
-    contexts: () => taskspending.find({tags: "largercontext"}).count(),
-    hardlandscape: () => taskspending.find({due: {$exists: 1}}).count(),
-    bothcontextandproject: () => taskspending.find({context: {$exists: 1}, project: {$exists: 1}}).count(),
-    contextonly: () => taskspending.find({context: {$exists: 1}, project: {$exists: 0}}).count(),
-    contextonlynotsomedaymaybe: () => taskspending.find({context: {$exists: 1}, project: {$exists: 0}, tags: {$nin: ["somedaymaybe"]}}).count(),
-    projectonly: () => taskspending.find({context: {$exists: 0}, project: {$exists: 1}}).count(),
-    noprojectorcontext: () => taskspending.find({context: {$exists: 0}, project: {$exists: 0}}).count(),
-    hardlandscapenoprojectorcontext: () => taskspending.find({due: {$exists: 1}, context: {$exists: 0}, project: {$exists: 0}}).count(),
-    hardlandscapeprojectonly: () => taskspending.find({due: {$exists: 1}, context: {$exists: 0}, project: {$exists: 1}}).count(),
-    hardlandscapecontextonly: () => taskspending.find({due: {$exists: 1}, context: {$exists: 1}, project: {$exists: 0}}).count(),
-    hardlandscapebothprojectandcontext: () => taskspending.find({due: {$exists: 1}, context: {$exists: 1}, project: {$exists: 1}}).count(),
-    nouuid: () => taskspending.find({uuid: {$exists: 0}}).count(),
-    nouuidorprojectorcontext: () => taskspending.find({uuid: {$exists: 0}, project: {$exists: 0}, context: {$exists: 0}}).count(),
-    nouuidprojectonly: () => taskspending.find({uuid: {$exists: 0}, project: {$exists: 1}, context: {$exists: 0}}).count(),
-    nouuidcontextonly: () => taskspending.find({uuid: {$exists: 0}, project: {$exists: 0}, context: {$exists: 1}}).count(),
-  },
-  NewCount: {
-    total: () => tasks.find().count(),
-    projects: () => tasks.find({type: "project"}).count(),
-    projectonly: () => tasks.find({context: {$exists: 0}, project: {$exists: 1}}).count(),
-    contexts: () => tasks.find({type: "context"}).count(),
-    contextonly: () => tasks.find({context: {$exists: 1}, project: {$exists: 0}}).count(),
-    hardlandscape: () => tasks.find({due: {$exists: 1}}).count(),
-    notaprojectorcontext: () => tasks.find({type: {$nin: ["context", "project"]}}).count(),
-    nouuid: () => tasks.find({uuid: {$exists: 0}}).count(),
-  },
-
 }
 
 export default feed
--- server/schema/feed.js
+++ server/schema/feed.js
@@ -1,73 +1,10 @@
 export const feed = `
 
-type Task {
-  _id: String
-  created: String
-  username: String
-  entry: String
-  status: String
-  due: String
-  type: String
-  description: String
-  uuid: String
-  project: String
-  context: String
-  super: String
-  workflow: String
-}
-
-type OldTask {
-  due: String
-  description: String
-  uuid: String
-  randomString: String
-}
-
-type BacklogTask {
-  due: String
-  description: String
-  uuid: String
-}
-
-type Count {
-  total: Int
-  projects: Int
-  contexts: Int
-  bothcontextandproject: Int
-  contextonly: Int
-  contextonlynotsomedaymaybe: Int
-  projectonly: Int
-  somedaymaybeproj: Int
-  somedaymaybecont: Int
-  somedaymaybeprojproject: Int
-  somedaymaybecontcontext: Int
-  noprojectorcontext: Int
-  hardlandscape: Int
-  hardlandscapenoprojectorcontext: Int
-  hardlandscapeprojectonly: Int
-  hardlandscapecontextonly: Int
-  hardlandscapebothprojectandcontext: Int
-  nouuid: Int
-  nouuidorprojectorcontext: Int
-  nouuidprojectonly: Int
-  nouuidcontextonly: Int
-}
-
-type NewCount {
-  total: Int
-  projects: Int
-  projectonly: Int
-  contexts: Int
-  contextonly: Int
-  hardlandscape: Int
-  notaprojectorcontext: Int
-  nouuid: Int
-}
-
 type MetaQuery {
   count: String
   subtotal: String
-  return: [RootQuery]
+  errors: [String]!
+  return: [RootQuery]!
 }
 
 type RootQuery {
@@ -84,27 +21,19 @@ type RootQuery {
   context: String
   super: String
   workflow: String
-
   firstfield: String
   secondfield: String
 }
 
 type Query {
-  metaquery(collection: String="tmpmutation", selector: String, limit: Int=0, skip: Int=0): MetaQuery
-  query(collection: String="tmpmutation", selector: String, limit: String): [RootQuery]
-  feed(limit: Int=1, skip: Int=0, _id: String, description: String, type: String, super: String, project: String, context: String, uuid: String): [Task]
-  oldfeed(limit: Int=1, skip: Int=0, description: String, duebefore: String, dueafter: String): [OldTask]
-  backlogfeed(limit: Int=1, description: String, duebefore: String): [BacklogTask]
-  count(collection: String="taskspending"): Count
-  newcount(collection: String="tasks"): NewCount
+  query(collection: String="tmpmutation", selector: String!, limit: Int=0, skip: Int=0): MetaQuery
+  _query(collection: String="tmpmutation", selector: String!, limit: String): [RootQuery]
   user(id: String!): User
 }
 
 type Mutation {
-  metamutate(collection: String="tmpmutation", op: String!, selector: String!, mutator: String): MetaMutate
-  mutate(collection: String="tmpmutation", op: String!, selector: String): [RootQuery]
-  feedinsert(firstfield: String, secondfield: String): FeedInsert
-  feedupdate(firstfield: String, secondfield: String): FeedUpdate
+  mutate(collection: String="tmpmutation", op: String!, selector: String!, mutator: String): MetaMutate
+  _mutate(collection: String="tmpmutation", op: String!, selector: String): [RootQuery]
 }
 
 type MetaMutate {
@@ -116,17 +45,7 @@ type MetaMutate {
   out: String
   mutator: String
   errors: [String]!
-  return: [RootQuery]
-}
-
-type FeedInsert {
-  firstfield: String
-  secondfield: String
-}
-
-type FeedUpdate {
-  firstfield: String
-  secondfield: String
+  return: [RootQuery]!
 }
 
 `;
