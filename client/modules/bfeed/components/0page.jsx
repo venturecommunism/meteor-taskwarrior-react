47dc33c7a7ee7767e1870441f1ece0f7ce93a3fb
Your Name <you@example.com>
2016-05-31 02:03:24 +0000

    getting closer to filling most data via props

--- client/modules/bfeed/components/0page.jsx
+++ client/modules/bfeed/components/0page.jsx
@@ -1,23 +1,9 @@
 import React from 'react';
 
 import Container from '../../core/containers/newcontainer'
-import Feed from './feed.jsx'
 
 import Data from './1data.jsx'
 const DataContainer = Container('data', Data)
-
-import CreateTask from './createtask.jsx'
-const CreateTaskContainer = Container('createtask', CreateTask)
-
-const FeedContainer = Container('feed', Feed)
-const PreviousCalendarContainer = Container('previouscalendar', Feed)
-
-import SideBar from './sidebar.jsx'
-const SideBarContainer = Container('sidebar', SideBar)
-
-const OverDueContainer = Container('overdue', Feed)
-
-const CalendarContainer = Container('calendar', Feed)
 
 export default class extends React.Component {
   render() {
@@ -25,6 +11,6 @@ export default class extends React.Component {
       <div className="bs-docs-section clearfix">
         <DataContainer />
       </div>
-    );
+    )
   }
 }
--- client/modules/bfeed/components/1data.jsx
+++ client/modules/bfeed/components/1data.jsx
@@ -6,18 +6,28 @@ import Feed from './feed.jsx'
 const FeedContainer = ActionsMapper('feed', Feed)
 //Feed used to be wrapped in a standard container with 'feed' as an argument
 
+import CreateTask from './createtask.jsx'
+const CreateTaskContainer = ActionsMapper('createtask', CreateTask)
+
+const PreviousCalendarContainer = ActionsMapper('previouscalendar', Feed)
+
+import SideBar from './sidebar.jsx'
+const SideBarContainer = ActionsMapper('sidebar', SideBar)
+
+const OverDueContainer = ActionsMapper('overdue', Feed)
+
+const CalendarContainer = ActionsMapper('calendar', Feed)
+
 export default ({data, actions}) => (
   <div className="row">
     <div className="col-md-3" style={{"backgroundColor": "gray"}}>
-   {/*   <SideBarContainer /> */}
+      <SideBarContainer data={data.sidebar} />
     </div>
     <div className="col-md-9">
-{/*
       <CalendarContainer />
       <CreateTaskContainer />
       <PreviousCalendarContainer />
       <OverDueContainer />
-*/}
       <FeedContainer data={data.feed} />
     </div>
   </div>
