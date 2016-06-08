12988bb90c59ea5b167d6bac88c699548b5da54c
Your Name <you@example.com>
2016-06-08 01:21:15 +0000

    removed some subscription logic

--- client/modules/bfeed/actions/1data.js
+++ client/modules/bfeed/actions/1data.js
@@ -8,77 +8,7 @@ export default {
       return {status: "completed", $and: [{tags: {$ne: "inbox"}}, {project: {$exists: false}}, {context: {$exists: false}}]}
     }
     function tasksselector() {
-      // get the URL contents
-      var queryParams = FlowRouter.current().queryParams
-      //sweetAlert("queryParams", queryParams)
-      //sweetAlert("queryParams.projects", queryParams.projects)
-
       var query = {}
-      query.feedquery = {}
-      query.projectsquery = {}
-      query.filtprojquery = {}
-      query.allprojectsandcontexts = {type: {$in: ["project", "context"]}}
-
-      switch (JSON.stringify(queryParams)) {
-        case "{}":
-          //sweetAlert("case", "{}")
-          query.feedquery = { type: {$nin: ['project', 'context']}, "workflow.status": 'inbox'}
-          query.projectsquery = { type : 'project', project: {$exists: 0} }
-          query.filtprojquery = { type : 'project', project: {$exists: 0}}
-          break
-        default:
-          query.feedquery = { type: {$nin: ['project', 'context']}}
-          query.projectsquery = { type: 'project', project: { $exists: 0} }
-          query.filtprojquery = { type: 'project' }
-      }
-
-      switch (queryParams.projects) {
-        case (null || ''):
-          break
-        default:
-          var project = queryParams.projects
-          query.feedquery = { project: project }
-          //sweetAlert("project", project)
-          query.filtprojquery = { type: 'project', project: project}
-      }
-
-      switch (queryParams.type) {
-        case (null || ''):
-          break
-        default:
-          var type = queryParams.type
-          //sweetAlert("type", type)
-          query.feedquery.type = { $in: [type] }
-      }
-
-      switch (Boolean(queryParams.projects && queryParams.type)) {
-        case (false):
-          break
-        default:
-          var project = queryParams.projects
-          var type = queryParams.type
-          query.feedquery = { project: project }
-          query.projectsquery = { type: type, project: project }
-          query.filtprojquery._id = {$ne: project}
-      }
-
-      switch (Boolean(!queryParams.projects && queryParams.type)) {
-        case (false):
-          break
-        default:
-          query.feedquery.project = { $exists: 0 }
-      }
-
-      switch(queryParams.mode) {
-        case ('do'):
-          query.feedquery["workflow.status"] = {$nin: ['project', 'inbox'] }
-          break
-        default:
-          query.feedquery["workflow.status"] = {$in: ['project', 'inbox'] }
-      }
-
-      //sweetAlert("query.feedquery.project", query.feedquery.project)
-      query = {$or: [query.feedquery, query.filtprojquery, query.projectsquery, query.allprojectsandcontexts]}
       return query
     }
 
