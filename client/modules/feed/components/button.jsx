a8556e6827375c1d476a9d43800d2a87070a49e8
Your Name <you@example.com>
2016-06-22 14:39:17 +0000

    misc

--- client/modules/feed/components/button.jsx
+++ client/modules/feed/components/button.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 export default ({...queryParams, data, actions}) => (
   <div key={queryParams.projects} className="universal-button">
-
     <div onClick={actions.buttonpress}>{actions.buttontext()}</div> <br />
   </div>
 )
