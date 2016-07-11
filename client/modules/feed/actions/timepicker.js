b87a8bf100e7c95595d6b4ecf75935e7ea06f7fc
Your Name <you@example.com>
2016-07-11 22:03:04 +0000

    calls an action

--- client/modules/feed/actions/timepicker.js
+++ client/modules/feed/actions/timepicker.js
@@ -1,8 +1,6 @@
 export default {
-  onChange({context}, dateString, {dateMoment, timestamp}) {
-    console.log(dateString)
-    console.log(dateMoment)
-    console.log(timestamp)
+  onChange({context}, data) {
+    console.log(data)
   },
 }
 
--- client/modules/feed/components/timepicker.jsx
+++ client/modules/feed/components/timepicker.jsx
@@ -3,22 +3,18 @@ import React from 'react'
 import 'react-date-picker/index.css'
 import { DateField, TransitionView, Calendar } from 'react-date-picker'
 
-const onChange = (dateString, { dateMoment, timestamp }) => {
-  console.log(dateString)
-  console.log(dateMoment)
-  console.log(timestamp)
-}
-
 export default ({actions, due, ...date, format}) => {
   return <div>{due}<DateField
     forceValidDate
-    value={date.moment ? date.moment : moment().format(format) }
+    value={due ? due : moment().format(format) }
     dateFormat={format}
+    updateOnDateClick={true}
+    onChange={actions.onChange.bind(this)}
   >
     <TransitionView>
       <Calendar 
         style={{padding: 10}}
-        onChange={onChange}
+        onChange={actions.onChange.bind(this)}
       />
     </TransitionView>
   </DateField>
