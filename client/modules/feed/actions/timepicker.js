bd45039ab55e22b2f36469150c6d9b59cb06b53e
Your Name <you@example.com>
2016-07-12 17:47:45 +0000

    makes datepicker not fire the times there's no datestring

--- client/modules/feed/actions/timepicker.js
+++ client/modules/feed/actions/timepicker.js
@@ -1,17 +1,19 @@
 export default {
   onChange({context}, data, id, dateString) {
-    formatteddata = {due: formattedMoment(moment(dateString)) }
-/*
-    Meteor.call('taskspending.update', formatteddata, id, (err) => {
-      if (err) {
-        sweetAlert("Oops...", err.message, "error")
-        return // LocalState.set('SAVING_ERROR', err.message);
-      }
-    });
-*/
-    console.log("data", dateString)
-    console.log("id", id)
-    console.log("formatteddata", formatteddata)
+    if (dateString) {
+      formatteddata = {due: formattedMoment(moment(dateString)) }
+
+      Meteor.call('taskspending.update', formatteddata, id, (err) => {
+        if (err) {
+          sweetAlert("Oops...", err.message, "error")
+          return // LocalState.set('SAVING_ERROR', err.message);
+        }
+      });
+
+      console.log("data", dateString)
+      console.log("id", id)
+      console.log("formatteddata", formatteddata)
+    }
   },
 }
 
