489ca19fcf0831460e227a825cdd82439e64c63c
anonymous <anonymous>
2016-07-29 07:50:07 -0400

    misc

--- client/modules/core/components/main_layout.js
+++ client/modules/core/components/main_layout.js
@@ -3,18 +3,7 @@ import Navigation from './navigation';
 
 const Layout = ({content = () => null }) => (
   <div>
-    <header>
-    <h1>Mantra Voice</h1>
-    <Navigation />
-    </header>
-
-    <div>
     {content()}
-    </div>
-
-    <footer>
-    <small>Built with <a href='https://github.com/kadirahq/mantra'>Mantra</a> &amp; Meteor.</small>
-    </footer>
   </div>
 );
 
