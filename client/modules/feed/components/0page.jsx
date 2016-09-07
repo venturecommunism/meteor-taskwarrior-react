77621b32ed3ecfab08247e4f7af9c7db9be0e9d8
anonymous <anonymous>
2016-09-07 17:34:26 -0400

    misc

--- client/modules/feed/components/0page.jsx
+++ client/modules/feed/components/0page.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 import Container from '../containers/container'
 
@@ -6,11 +6,10 @@ import Data from './1data.jsx'
 const DataContainer = Container(Data)
 
 const App = ({queryParams}) => (
-      <div className="clearfix">
-        <DataContainer {...queryParams} />
-      </div>
-    )
-
+  <div className="clearfix">
+    <DataContainer {...queryParams} />
+  </div>
+)
 
 export default App
 
