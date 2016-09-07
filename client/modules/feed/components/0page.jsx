a5d7cc7b9820aae56d710d1b8a3c6d42232ca4a0
anonymous <anonymous>
2016-09-07 19:29:17 -0400

    test prior to passing date down as prop

--- client/modules/feed/components/0page.jsx
+++ client/modules/feed/components/0page.jsx
@@ -5,11 +5,47 @@ import Container from '../containers/container'
 import Data from './1data.jsx'
 const DataContainer = Container(Data)
 
+import { compose } from 'react-komposer'
+
+// test 
+
+// Create a component to display Time
+const Time = ({queryParams, time}) => (
+	<div>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+{time}
+<DataContainer {...queryParams} />
+  </div>
+)
+
+// Create the composer function and tell how to fetch data
+const composerFunction = (props, onData) => {
+
+  const now = () => (
+    props.timestamp ? Date.now() : new Date().toString()
+  )
+	
+  const handler = setInterval(() => {
+    const time = now()
+    onData(null, {time})
+  }, 100)
+  
+  const cleanup = () => clearInterval(handler)
+  return cleanup
+}
+
+// Compose the container
+const Clock = compose(composerFunction)(Time)
+
+// test 
+
 const App = ({queryParams}) => (
   <div className="clearfix">
     <DataContainer {...queryParams} />
   </div>
 )
 
-export default App
-
+export default Clock
